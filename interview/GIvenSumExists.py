6c30654f89aa2652a4a8976a819822c1512b2160
Rajarajan <rrn.rajarajan@gmail.com>
2024-01-03 14:04:04 +0530

    Updated

--- interview/GIvenSumExists.py
+++ interview/GIvenSumExists.py
@@ -1,13 +1,13 @@
-# Check if pair with given Sum exists in Array (Two Sum)
+# Check if pair with given Sum exists in Array
 def Sum(arr, target):
-    n = len(arr) - 1
+    n = len(arr) 
     
     for i in range(n):
         for j in (i+1, n):
             if (i+j) == target:
                 print(f"The target {target} meets when adding {i} and {j}.")
-                return
-    return 0
+    #             return
+    # return 0
 
 usr = int(input("How many elements do you want add? "))
 arr=[]
