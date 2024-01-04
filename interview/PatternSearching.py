c70a8bbe44a23fa50cdd48facc1a07086737a372
Rajarajan <rrn.rajarajan@gmail.com>
2024-01-04 16:40:22 +0530

    Update PatternSearching.py

--- interview/PatternSearching.py
+++ interview/PatternSearching.py
@@ -4,9 +4,11 @@ def pattern_search(text, pattern):
 
     # Slide the pattern over the text
     for i in range(t_len - p_len + 1):
+        # print(i)
         match = True
         # Check for a match starting from the current index i
         for j in range(p_len):
+            # print(text[i+j], pattern[j])
             if text[i + j] != pattern[j]:
                 match = False
                 break
@@ -17,4 +19,25 @@ def pattern_search(text, pattern):
 # Example usage:
 text = "ABCCDDAEFG"
 pattern = "CDD"
-pattern_search(text, pattern)
+pattern_search(text, pattern) # 3
+
+# 0
+# A C
+# 1
+# B C
+# 2
+# C C
+# C D
+# 3
+# C C
+# D D
+# D D
+# Pattern found at index 3
+# 4
+# D C
+# 5
+# D C
+# 6
+# A C
+# 7
+# E C
