563ac3b702d19cc6a9e24618eae8b14ca1733da5
Rajarajan <rrn.rajarajan@gmail.com>
2024-01-04 05:35:48 +0530

    Update Subset.py

--- Hash DataStructure/Questions/Subset.py
+++ Hash DataStructure/Questions/Subset.py
@@ -3,12 +3,14 @@ def isSubset(arr1, arr2):
     hash_table = {}
     
     for element in arr1:
-        hash_table[element] = True
+        hash_table[element] = True # It will save true for all key present in arr1
         
     for i in arr2:
         if i not in hash_table:
             return False
-    return True
+    return hash_table.values   # True
+    
+    
 
 array1 = [1, 2, 3, 4, 5, 6]
 array2 = [3, 4, 5]
@@ -17,3 +19,4 @@ if isSubset(array1, array2):
     print("Array2 is a subset of array 1")
 else:
     print("Array2 is not a subset of Array1")
+
