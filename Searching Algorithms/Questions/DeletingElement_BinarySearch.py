6448bac070fb74738e881bf280821f96ad79e4b3
Rajarajan <rrn.rajarajan@gmail.com>
2024-02-01 13:45:24 -0500

    Deleting element using binary search

---
+++ Searching Algorithms/Questions/DeletingElement_BinarySearch.py
@@ -0,0 +1,26 @@
+def BinarySearch(arr, element):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        
+        mid = (low+high) // 2
+        
+        if arr[mid] == element:
+            # Element found, delete it
+            i = mid
+            while i < len(arr)-1:
+               arr[i] = arr[i+1]        # Shift elements to the left
+               i += 1
+            arr.pop()           # Remove the last element
+            return True         # Return True to indicate successful deletion
+        elif arr[mid] < element:
+            low = mid + 1
+        else: high = mid - 1
+        
+    return False
+
+
+arr = [2, 4, 6, 7, 8, 10, 12]
+element = 6
+print(BinarySearch(arr, element))
