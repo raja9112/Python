dcc3e309791381c3d270c038fdbd63d6653f687d
Rajarajan <rrn.rajarajan@gmail.com>
2024-02-01 13:44:20 -0500

    Inserting element using binary search

---
+++ Searching Algorithms/Questions/InsertElement_BinarySearch.py
@@ -0,0 +1,33 @@
+def BinarySearch(arr, element):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        
+        mid = (low+high) // 2
+        
+        if arr[mid] == element:
+            return mid
+        if arr[mid] > element:
+            high = mid - 1
+        else: low = mid + 1
+        
+    # print (low) # 3
+    
+    
+    # The target is not found, so it should be inserted at the position of low
+    arr.append(None)    # Add a placeholder to extend the array
+    n = len(arr) - 1
+    while n > low:
+        arr[n] = arr[n-1]   # Shift elements to the right
+        n -= 1
+    arr[low] = element  # Insert the element at the correct position
+    return low          # Return the index where the element is inserted
+
+
+    # Method 2
+    #  arr.insert(low, target)        # array.insert(index, element)
+
+arr = [2, 4, 6, 8, 10, 12]
+element = 7
+print(BinarySearch(arr, element))
